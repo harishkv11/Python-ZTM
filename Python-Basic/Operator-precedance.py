1b21398c368a1b8401379ad8b21048c52d087bce
Harish <iamharish.maniax@gmail.com>
2021-11-19 14:33:20 +0530

    Added File

---
+++ Python-Basic/Operator-precedance.py
@@ -0,0 +1,17 @@
+#Operator Precedence -> The order by which operators must be executed
+print(20-2*4) #12 not 72
+
+#Order -> From Top to bottom in decreasing precedence
+# ()	Parentheses
+# **	Exponent
+# +x, -x, ~x	Unary plus, Unary minus, Bitwise NOT
+# *, /, //, %	Multiplication, Division, Floor division, Modulus
+# +, -	Addition, Subtraction
+# <<, >>	Bitwise shift operators
+# &	Bitwise AND
+# ^	Bitwise XOR
+# |	Bitwise OR
+# ==, !=, >, >=, <, <=, is, is not, in, not in	Comparisons, Identity, Membership operators
+# not	Logical NOT
+# and	Logical AND
+# or	Logical OR
