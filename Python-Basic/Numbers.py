853dc4b8bd0a6db645baf1526639e606225fb50e
Harish <iamharish.maniax@gmail.com>
2021-11-19 14:25:57 +0530

    Added File

---
+++ Python-Basic/Numbers.py
@@ -0,0 +1,17 @@
+#Arithmetic Operations
+print(10+4) #Addition
+print(10-4) #Subtraction
+print(10*4) #Multiplication
+print(10/4) #Division (Floating)
+
+print(10//4) #Division (Integer)
+
+#type() -> returns the type of input
+print(type(3+4)) #int
+print(type(1+1.9)) #int+float=float
+
+#Exponentition (base**power)
+print(2**3)
+
+#Modulo -> Returns the remainder
+print(10%4)
