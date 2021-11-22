d2cfb3fe1e57b02363994cf2cc84fb3374cb190f
Harish <iamharish.maniax@gmail.com>
2021-11-22 21:55:31 +0530

    Added Binary Function

--- Python-Basic/Math-Functions.py
+++ Python-Basic/Math-Functions.py
@@ -5,3 +5,6 @@ print(round(3.7))
 #abs() -> Returns the absolute value of the input
 print(abs(1))
 print(abs(-44))
+
+#bin() -> returns a string binary representation of a number
+print(bin(4)) #0b100
