0181c3b90176ca15c5a200c225710c2ab42d7ca9
Harish <iamharish.maniax@gmail.com>
2021-11-24 21:30:51 +0530

    Added File

---
+++ Python-Basic/Variables.py
@@ -0,0 +1,31 @@
+#Used to store the information on computer
+name = "harish"
+
+#Can be changed or reassigned a new value
+name = "harish"
+name = "harshit" #Changed
+
+first_name = name #reassigned
+
+#Case Sensitive
+name = "harish"
+print(name) #Output --> harish
+print(NAME) #No output
+
+
+#Rules for naming variables:
+    1. Must start with lowercase or underscore
+    2. Can contain lowercase, uppercase, numbers, underscore
+    3. Must not contain spaces
+    4. Must not overwrite pre defined keywords
+
+#Constant
+    --> These values should not be changed
+    --> convention is to use Uppercase for naming constants
+    --> eg PI = 3.14
+
+#Two rapidly assign values
+a,b,c = 1,2,3
+print(a) #1
+print(b) #2
+print(c) #3
