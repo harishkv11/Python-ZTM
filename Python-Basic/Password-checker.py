1e76b880dbd8c53e7a718a27f758fb1d11c6bacb
Harish <iamharish.maniax@gmail.com>
2021-12-03 00:21:05 +0530

    Added File

---
+++ Python-Basic/Password-checker.py
@@ -0,0 +1,7 @@
+username = input("What is your username? ")
+password = input("What is your password? ")
+
+length = len(password)
+encrypted_password = '*'*length
+
+print(f'username, your password {encrypted_password} is {length} letters long ')
