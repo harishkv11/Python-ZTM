ddc8b0f38022bb63cd120d446fa02d2e04dcb7fb
Harish <iamharish.maniax@gmail.com>
2021-12-03 00:25:24 +0530

    Added File

---
+++ Python-Basic/List.py
@@ -0,0 +1,13 @@
+#Denoted by square brackets []
+li = [1,2,3,4,5]
+li2 = ['a','b','c']
+li3 = [1,2,'a',True] #Mixed data types
+
+#Data Structure --> Used to store the data
+
+amazon_cart = ['notebooks', 'sunglasses']
+
+#Items can be accessed with index
+print(amazon_cart[0])
+
+#Accesing any items out of index gives indexError
