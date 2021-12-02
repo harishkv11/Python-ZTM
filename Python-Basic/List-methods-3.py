406d7f0b476291b07afa1e26329a4a8cc856a4de
Harish <iamharish.maniax@gmail.com>
2021-12-03 01:00:53 +0530

    Added File

---
+++ Python-Basic/List-methods-3.py
@@ -0,0 +1,18 @@
+basket = [3,2,5,1,6,7]
+
+#sort --> Sorts the list in ascending order
+basket.sort()
+print(basket)
+
+#sorted --> Produces a new sorted list, doesn't modifies the list
+basket = [3,2,5,1,6,7]
+print(sorted(basket))
+print(basket)
+
+#copy --> creates a copy of list
+new_list = basket.copy()
+print(new_list)
+
+#reverse --> Reverses the list
+basket.reverse()
+print(basket)
